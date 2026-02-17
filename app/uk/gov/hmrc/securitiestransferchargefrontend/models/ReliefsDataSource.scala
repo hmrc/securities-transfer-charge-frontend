24657dc44ff0842b332c52b820d29a50f88a4813
ttiyemba <57238297+ttiyemba@users.noreply.github.com>
2026-02-17 17:16:09 +0000

    get rate method

--- app/uk/gov/hmrc/securitiestransferchargefrontend/models/ReliefsDataSource.scala
+++ app/uk/gov/hmrc/securitiestransferchargefrontend/models/ReliefsDataSource.scala
@@ -42,4 +42,11 @@ class ReliefsDataSource @Inject()(
         }
       }
       .toSeq
+
+
+  def getRate(reliefName: String): Int =
+    reliefs
+      .find(_.name == reliefName)
+      .map(_.rate)
+      .getOrElse(throw new NoSuchElementException(s"No rate found for relief: $reliefName"))
 }
