22ee37121c521093284604f0c120daaddfaabfb8
zarifskmustafizur-ggs <237820465+zarifskmustafizur-ggs@users.noreply.github.com>
2026-02-18 12:39:46 +0000

    Fixed the test

--- test/controllers/WhatReliefAreYouApplyingForControllerSpec.scala
+++ test/controllers/WhatReliefAreYouApplyingForControllerSpec.scala
@@ -98,7 +98,7 @@ class WhatReliefAreYouApplyingForControllerSpec extends SpecBase with MockitoSug
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        redirectLocation(result).value mustEqual routes.SecuritiesTargetController.onPageLoad(NormalMode).url
       }
     }
 
