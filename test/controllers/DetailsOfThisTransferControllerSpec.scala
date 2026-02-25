2b886d83a3181c94acc266d7030b321d8770c3c7
ttiyemba <57238297+ttiyemba@users.noreply.github.com>
2026-02-25 09:56:01 +0000

    navigation

--- test/controllers/DetailsOfThisTransferControllerSpec.scala
+++ test/controllers/DetailsOfThisTransferControllerSpec.scala
@@ -101,7 +101,7 @@ class DetailsOfThisTransferControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        redirectLocation(result).value mustEqual routes.CheckYourAnswersController.onPageLoad().url
       }
     }
 
