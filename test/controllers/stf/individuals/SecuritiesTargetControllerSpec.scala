c834d47954c9a4074685ad447a4a0a67a48d56da
wp94 <43645818+wp94@users.noreply.github.com>
2026-03-04 18:26:01 +0000

    undo more capitalisations

--- test/controllers/stf/individuals/SecuritiesTargetControllerSpec.scala
+++ test/controllers/stf/individuals/SecuritiesTargetControllerSpec.scala
@@ -126,7 +126,7 @@ class SecuritiesTargetControllerSpec extends SpecBase with MockitoSugar {
       running(application) {
         val request =
           FakeRequest(POST, securitiesTargetRoute)
-            .withFormUrlEncodedBody(("BusinessName", "value 1"), ("crn", "value 2"))
+            .withFormUrlEncodedBody(("businessName", "value 1"), ("crn", "value 2"))
 
         val result = route(application, request).value
 
