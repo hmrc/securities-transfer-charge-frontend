41d182ec5a235c822b3d701865df62c993d84c3c
zarifskmustafizur-ggs <237820465+zarifskmustafizur-ggs@users.noreply.github.com>
2026-03-18 12:22:31 +0000

    Added the controller, view and the page with all the routes and the messages

---
+++ app/uk/gov/hmrc/securitiestransferchargefrontend/controllers/stf/individuals/FormattingErrorController.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.securitiestransferchargefrontend.controllers.stf.individuals
+
+import controllers.Execution.trampoline
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import uk.gov.hmrc.securitiestransferchargefrontend.clients.SubmissionIdClient
+import uk.gov.hmrc.securitiestransferchargefrontend.controllers.actions.*
+import uk.gov.hmrc.securitiestransferchargefrontend.models.{NormalMode, UserAnswers}
+import uk.gov.hmrc.securitiestransferchargefrontend.navigation.Navigator
+import uk.gov.hmrc.securitiestransferchargefrontend.pages.FormattingErrorPage
+import uk.gov.hmrc.securitiestransferchargefrontend.views.html.stf.individuals.FormattingErrorView
+
+import javax.inject.{Inject, Named}
+
+class FormattingErrorController @Inject()(
+                                       override val messagesApi: MessagesApi,
+                                       @Named("individuals") navigator: Navigator,
+                                       stcAuthEnrolled: StcAuthEnrolledAction,
+                                       val controllerComponents: MessagesControllerComponents,
+                                       view: FormattingErrorView,
+                                       idClient: SubmissionIdClient,
+                                     ) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad: Action[AnyContent] = stcAuthEnrolled {
+    implicit request =>
+      Ok(view())
+  }
+
+  def onSubmit(): Action[AnyContent] = stcAuthEnrolled.async {
+    implicit request =>
+      val userId = request.internalId
+
+      for {
+        submissionId <- idClient.nextSubmissionId()
+        nextPage <- navigator.nextPage(FormattingErrorPage, NormalMode, UserAnswers.empty(userId)(submissionId))
+      } yield Redirect(nextPage)
+  }
+}
+
+
+
