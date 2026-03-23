817cd5f1562af6985ceeaaf2f072408317c88901
ttiyemba <57238297+ttiyemba@users.noreply.github.com>
2026-03-23 11:32:28 +0000

    fix urls

--- app/uk/gov/hmrc/securitiestransferchargefrontend/config/FrontendAppConfig.scala
+++ app/uk/gov/hmrc/securitiestransferchargefrontend/config/FrontendAppConfig.scala
@@ -106,11 +106,10 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
 
   lazy val connectedPersonsInformationUrl: String = configuration.get[String]("urls.external.connectedPersonsInformation")
 
-  val baseUrl: String = servicesConfig.baseUrl("securities-transfer-charge-frontend")
   val basePath = "/securities-transfer-charge"
   
   val upscanBaseUrl: String = servicesConfig.baseUrl("upscan-initiate")
-  val upscanCallbackUrl: String =  s"$baseUrl$basePath/stf/upscan-callback"
-  val upscanUploadSuccessfulUrl: String = configuration.get[String]("upscan.success")
-  val upscanUploadFailureUrl: String = configuration.get[String]("upscan.error")
+  val upscanCallbackUrl: String =  s"$host$basePath/stf/upscan-callback"
+  val upscanUploadSuccessfulUrl: String = s"$host$basePath/stf/file-uploaded"
+  val upscanUploadFailureUrl: String = s"$host$basePath/stf/file-upload/problem"
 }
