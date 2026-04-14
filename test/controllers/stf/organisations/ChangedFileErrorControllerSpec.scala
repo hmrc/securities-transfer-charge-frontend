87386133133eae2f8acd15f5b67ddb3f05e00be6
zarifskmustafizur-ggs <237820465+zarifskmustafizur-ggs@users.noreply.github.com>
2026-04-14 14:01:51 +0100

    Fixed the error

--- test/controllers/stf/organisations/ChangedFileErrorControllerSpec.scala
+++ test/controllers/stf/organisations/ChangedFileErrorControllerSpec.scala
@@ -19,8 +19,8 @@ package controllers.stf.organisations
 import base.SpecBase
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
-import uk.gov.hmrc.securitiestransferchargefrontend.controllers.stf.individuals.routes
-import uk.gov.hmrc.securitiestransferchargefrontend.views.html.stf.individuals.ChangedFileErrorView
+import uk.gov.hmrc.securitiestransferchargefrontend.controllers.stf.individuals.bulk.routes
+import uk.gov.hmrc.securitiestransferchargefrontend.views.html.stf.individuals.bulk.ChangedFileErrorView
 
 class ChangedFileErrorControllerSpec extends SpecBase {
 
