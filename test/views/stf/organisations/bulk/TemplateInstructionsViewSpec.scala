0bc1eb3899bf1a848bcb6f5cbc32488d1d964130
tharunprasath <65491423+tharunprasath@users.noreply.github.com>
2026-04-16 15:09:05 +0100

    STOSB-1549 [FE] Bus- STF Bulk w/o C&D- Build Page 1- 'How to use the template'

---
+++ test/views/stf/organisations/bulk/TemplateInstructionsViewSpec.scala
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.stf.organisations.bulk
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.Application
+import uk.gov.hmrc.securitiestransferchargefrontend.controllers.stf.shared.routes
+import uk.gov.hmrc.securitiestransferchargefrontend.views.html.stf.organisations.bulk.TemplateInstructionsView
+import views.ViewBaseSpec
+
+class TemplateInstructionsViewSpec extends ViewBaseSpec {
+
+  override def fakeApplication(): Application = applicationBuilder().build()
+
+  private val viewInstance         = app.injector.instanceOf[TemplateInstructionsView]
+
+  def view(): Document = Jsoup.parse(
+    viewInstance()(fakeRequest, messages).body
+  )
+
+  object ExpectedContent {
+    val title = messages("org.templateInstructions.title")
+    val heading = messages("org.templateInstructions.heading")
+    val caption: String = messages("transfer.details.caption")
+
+    val para1Value = messages("org.templateInstructions.p1")
+
+    val step1 = messages("org.templateInstructions.step1")
+    val downloadHref = "/securities-transfer-charge/assets/Bulk_Securities_Transfer_Charge_template_v1b.xlsx"
+    val downloadFileName = "Bulk Securities Transfer Charge template v1b.xlsx"
+
+    val step2 = messages("org.templateInstructions.step2")
+    val step3 = messages("org.templateInstructions.step3")
+    val para2Value = messages("org.templateInstructions.p2")
+    val para3Span1 = messages("org.templateInstructions.p3.span1")
+
+    val continue = messages("site.continue")
+    val returnLink = messages("org.return-to-dashboard.link")
+  }
+
+  "The TemplateInstructionsView" - {
+    "the user is an Individual" - {
+      val templateInstructionsView = view()
+
+      "have the correct title" in {
+        templateInstructionsView.title must include(ExpectedContent.title)
+      }
+
+      "have the correct heading" in {
+        templateInstructionsView.select("h1").text() mustBe ExpectedContent.heading
+      }
+
+      "display the correct caption text" in {
+        templateInstructionsView.getElementsByClass("govuk-caption-l").text() mustBe ExpectedContent.caption
+      }
+
+      "display the correct of first paragraph content" in {
+        templateInstructionsView.para(1) mustBe Some(ExpectedContent.para1Value)
+      }
+
+      "display a numbered list with 3 items" in {
+        templateInstructionsView.select("ol.govuk-list.govuk-list--number li").size() mustBe 3
+      }
+
+      "display the correct step 1 download link" in {
+        val downloadLink = templateInstructionsView.select("ol.govuk-list.govuk-list--number li a.govuk-link").first()
+        downloadLink.text() mustBe ExpectedContent.step1
+        downloadLink.attr("href") mustBe ExpectedContent.downloadHref
+        downloadLink.hasAttr("download") mustBe true
+        downloadLink.attr("download") mustBe ExpectedContent.downloadFileName
+      }
+
+      "display the correct step 2 text" in {
+        templateInstructionsView.select("ol.govuk-list.govuk-list--number li").get(1).text() mustBe ExpectedContent.step2
+      }
+
+      "display the correct step 3 text" in {
+        templateInstructionsView.select("ol.govuk-list.govuk-list--number li").get(2).text() mustBe ExpectedContent.step3
+      }
+
+      "display the correct second paragraph" in {
+        templateInstructionsView.para(2) mustBe Some(ExpectedContent.para2Value)
+      }
+
+      "display the correct third paragraph with 'STC dashboard' link that takes user back to the submission dashboard page" in {
+        val paragraph = templateInstructionsView.select("p.govuk-body").last()
+        val spans = paragraph.select("span")
+        spans.get(0).text() mustBe ExpectedContent.para3Span1
+      }
+
+
+      "have a continue button" in {
+        val continueButton = templateInstructionsView.select(".govuk-button").first()
+        continueButton.text() mustBe ExpectedContent.continue
+      }
+
+      "have a link to return back to the submission dashboard page" in {
+        val returnLink = templateInstructionsView.select(".govuk-button-group a.govuk-link").first()
+        returnLink.text() mustBe ExpectedContent.returnLink
+        returnLink.attr("href") mustBe routes.SubmissionsDashboardController.onPageLoad().url
+      }
+    }
+  }
+
+}
