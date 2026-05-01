97be50d78dbec0a577cee9e8123057aa65346dd6
zarifskmustafizur-ggs <237820465+zarifskmustafizur-ggs@users.noreply.github.com>
2026-05-01 18:20:17 +0100

    Merge branch 'main' into STOSB-1631
    # Conflicts:
    #	app/uk/gov/hmrc/securitiestransferchargefrontend/navigation/stf/agents/ForwardRoutes.scala
    #	conf/app.routes
    #	test/controllers/stf/agents/single/NameOfSellerControllerSpec.scala

--- app/uk/gov/hmrc/securitiestransferchargefrontend/navigation/stf/agents/ForwardRoutes.scala
+++ app/uk/gov/hmrc/securitiestransferchargefrontend/navigation/stf/agents/ForwardRoutes.scala
@@ -25,7 +25,7 @@ import uk.gov.hmrc.securitiestransferchargefrontend.models.{NormalMode, UserAnsw
 import uk.gov.hmrc.securitiestransferchargefrontend.navigation.PersistentNavigationHelper
 import uk.gov.hmrc.securitiestransferchargefrontend.pages.Page
 import uk.gov.hmrc.securitiestransferchargefrontend.pages.stf.shared.{HowToNotifyAboutSecuritiesTransferPage, SubmissionsDashboardPage}
-import uk.gov.hmrc.securitiestransferchargefrontend.pages.stf.single.{AgentReferencePage, NameOfSellerPage}
+import uk.gov.hmrc.securitiestransferchargefrontend.pages.stf.single.{AgentReferencePage, ConnectedPersonsPage, NameOfSellerPage, StfBuyersAddressPage, StfSellerAddressPage}
 import uk.gov.hmrc.securitiestransferchargefrontend.services.AnswerPersistenceService
 
 import scala.concurrent.{ExecutionContext, Future}
