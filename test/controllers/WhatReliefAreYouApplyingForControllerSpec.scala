7b1e5822440e06b30cccd587cd7f9d83119c2d44
jon-humble-hmrc <194776673+jon-humble-hmrc@users.noreply.github.com>
2026-07-10 11:25:29 +0100

    Tests

--- test/controllers/WhatReliefAreYouApplyingForControllerSpec.scala
+++ test/controllers/WhatReliefAreYouApplyingForControllerSpec.scala
@@ -1,19 +1,36 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
 import uk.gov.hmrc.securitiestransferchargefrontend.models.{NormalMode, UserAnswers}
-import uk.gov.hmrc.securitiestransferchargefrontend.navigation.{FakeNavigator, Navigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
-import uk.gov.hmrc.securitiestransferchargefrontend.pages.WhatReliefAreYouApplyingForViewPage
 import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
-import play.api.test.Helpers._
+import play.api.test.Helpers.*
 import uk.gov.hmrc.securitiestransferchargefrontend.forms.sh03.organisations.single.WhatReliefAreYouApplyingForFormProvider
 import uk.gov.hmrc.securitiestransferchargefrontend.repositories.SessionRepository
-import uk.gov.hmrc.securitiestransferchargefrontend.views.html.WhatReliefAreYouApplyingForViewView
+import uk.gov.hmrc.securitiestransferchargefrontend.pages.sh03.WhatReliefAreYouApplyingForPage
+import uk.gov.hmrc.securitiestransferchargefrontend.controllers.sh03.organisations.routes as sh03orgRoutes
+import uk.gov.hmrc.securitiestransferchargefrontend.controllers.routes
+import uk.gov.hmrc.securitiestransferchargefrontend.navigation.Navigator
 
 import scala.concurrent.Future
 
@@ -24,7 +41,7 @@ class WhatReliefAreYouApplyingForControllerSpec extends SpecBase with MockitoSug
   val formProvider = new WhatReliefAreYouApplyingForFormProvider()
   val form = formProvider()
 
-  lazy val whatReliefAreYouApplyingForViewRoute = routes.WhatReliefAreYouApplyingForViewController.onPageLoad(NormalMode).url
+  lazy val whatReliefAreYouApplyingForViewRoute = routes.WhatReliefAreYouApplyingForController.onPageLoad(NormalMode).url
 
   "WhatReliefAreYouApplyingForView Controller" - {
 
@@ -37,7 +54,7 @@ class WhatReliefAreYouApplyingForControllerSpec extends SpecBase with MockitoSug
 
         val result = route(application, request).value
 
-        val view = application.injector.instanceOf[WhatReliefAreYouApplyingForViewView]
+        val view = application.injector.instanceOf[WhatReliefAreYouApplyingForView]
 
         status(result) mustEqual OK
         contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
@@ -46,14 +63,14 @@ class WhatReliefAreYouApplyingForControllerSpec extends SpecBase with MockitoSug
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(WhatReliefAreYouApplyingForViewPage, "answer").success.value
+      val userAnswers = UserAnswers(userAnswersId).set(WhatReliefAreYouApplyingForPage, "answer").success.value
 
       val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
       running(application) {
         val request = FakeRequest(GET, whatReliefAreYouApplyingForViewRoute)
 
-        val view = application.injector.instanceOf[WhatReliefAreYouApplyingForViewView]
+        val view = application.injector.instanceOf[WhatReliefAreYouApplyingForView]
 
         val result = route(application, request).value
 
@@ -99,7 +116,7 @@ class WhatReliefAreYouApplyingForControllerSpec extends SpecBase with MockitoSug
 
         val boundForm = form.bind(Map("value" -> ""))
 
-        val view = application.injector.instanceOf[WhatReliefAreYouApplyingForViewView]
+        val view = application.injector.instanceOf[WhatReliefAreYouApplyingForView]
 
         val result = route(application, request).value
 
--- test/forms/sh03/organisations/single/WhatReliefAreYouApplyingForFormProviderSpec.scala
+++ test/forms/sh03/organisations/single/WhatReliefAreYouApplyingForFormProviderSpec.scala
@@ -1,32 +1,42 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package forms.sh03.organisations.single
 
 import play.api.data.FormError
-import uk.gov.hmrc.securitiestransferchargefrontend.forms.behaviours.StringFieldBehaviours
+import forms.behaviours.StringFieldBehaviours
+import uk.gov.hmrc.securitiestransferchargefrontend.forms.sh03.organisations.single.WhatReliefAreYouApplyingForFormProvider
 
 class WhatReliefAreYouApplyingForFormProviderSpec extends StringFieldBehaviours {
 
-  val requiredKey = "whatReliefAreYouApplyingFor.error.required"
-  val lengthKey = "whatReliefAreYouApplyingFor.error.length"
-  val maxLength = 100
+  val requiredKey = "org.sh03.whatReliefAreYouApplyingFor.error.required"
+  val lengthKey = "org.sh03.whatReliefAreYouApplyingFor.error.length"
 
   val form = new WhatReliefAreYouApplyingForFormProvider()()
 
-  ".value" - {
+  ".reliefs" - {
 
-    val fieldName = "value"
+    val fieldName = "reliefs"
 
     behave like fieldThatBindsValidData(
       form,
       fieldName,
-      stringsWithMaxLength(maxLength)
+      "value"
     )
 
-    behave like fieldWithMaxLength(
-      form,
-      fieldName,
-      maxLength = maxLength,
-      lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
-    )
 
     behave like mandatoryField(
       form,
