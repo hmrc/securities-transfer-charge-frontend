6e47a79bf8d6ea1d36bf8c47b19c08c1a4629072
tharunprasath <65491423+tharunprasath@users.noreply.github.com>
2026-08-13 12:27:26 +0100

    STOSB-1862 Issue fix for backroutes from checkRoutes

--- app/uk/gov/hmrc/securitiestransferchargefrontend/navigation/stf/individuals/BackwardsRoutes.scala
+++ app/uk/gov/hmrc/securitiestransferchargefrontend/navigation/stf/individuals/BackwardsRoutes.scala
@@ -81,6 +81,8 @@ class BackwardsRoutes(defaultPage: Call):
     case WhatReliefAreYouApplyingForPage => _ => individualSingleRoutes.ApplyingForReliefController.onPageLoad(CheckMode)
     case DetailsOfThisTransferPage => _ => individualSingleRoutes.PurchasingSharesController.onPageLoad(CheckMode)
     case OtherSecuritiesTypePage => _ => individualSingleRoutes.PurchasingSharesController.onPageLoad(CheckMode)
+    case TotalMarketValuePage => _ => individualSingleRoutes.PurchasingSharesController.onPageLoad(CheckMode)
+    case AmountPaidForSecuritiesPage => _ => individualSingleRoutes.PurchasingSharesController.onPageLoad(CheckMode)
     case PurchasingSharesPage => _ => individualSingleRoutes.ConnectedPersonsController.onPageLoad(CheckMode)
     case _ => _ => stfSingleCyaRoutes.CheckYourAnswersController.onPageLoad()
   }
--- app/uk/gov/hmrc/securitiestransferchargefrontend/navigation/stf/organisations/BackwardsRoutes.scala
+++ app/uk/gov/hmrc/securitiestransferchargefrontend/navigation/stf/organisations/BackwardsRoutes.scala
@@ -67,6 +67,8 @@ class BackwardsRoutes(defaultPage: Call):
     case WhatReliefAreYouApplyingForPage => _ => orgSingleRoutes.ApplyingForReliefController.onPageLoad(CheckMode)
     case DetailsOfThisTransferPage => _ => orgSingleRoutes.PurchasingSharesController.onPageLoad(CheckMode)
     case OtherSecuritiesTypePage => _ => orgSingleRoutes.PurchasingSharesController.onPageLoad(CheckMode)
+    case TotalMarketValuePage => _ => orgSingleRoutes.PurchasingSharesController.onPageLoad(CheckMode)
+    case AmountPaidForSecuritiesPage => _ => orgSingleRoutes.PurchasingSharesController.onPageLoad(CheckMode)
     case PurchasingSharesPage => _ => orgSingleRoutes.ConnectedPersonsController.onPageLoad(CheckMode)
     case _ => _ => stfSingleCyaRoutes.CheckYourAnswersController.onPageLoad()
   }
